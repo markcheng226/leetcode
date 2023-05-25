7653bbaa17d9ae180570ec73bbfcfdc2a9feb6be
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2023-05-25 15:13:57 -0400

    Time: 108 ms (67.13%) | Memory: 19.8 MB (47.58%) - LeetSync

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,11 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) < 2:
+        if len(strs)<2:
             return [strs]
-
-        result={}
+        
+        result = {}
         for s in strs:
             temp = ''.join(sorted(s))
             result[temp]=result.get(temp,[])+[s]
         return result.values()
-        
