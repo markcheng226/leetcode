0492fc9fd0e2fd270036ae71d71a85cc8f4c4419
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2023-05-25 15:12:27 -0400

    Time: 113 ms (54.75%) | Memory: 19.8 MB (47.58%) - LeetSync

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -3,11 +3,9 @@ class Solution:
         if len(strs) < 2:
             return [strs]
 
-        result = {}
+        result={}
         for s in strs:
             temp = ''.join(sorted(s))
             result[temp]=result.get(temp,[])+[s]
         return result.values()
-
-        
         
