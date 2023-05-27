1eb13c780ddb30c3b90fa502c5ecfc4d3cdf6fbb
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2023-05-27 13:17:22 -0400

    Time: 68 ms (36.13%) | Memory: 16.7 MB (55.64%) - LeetSync

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -2,13 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
         result = [0]*26
-                
+
         for i in s:
             result[ord(i)-ord("a")] +=1
+        
         for i in t:
             result[ord(i)-ord("a")] -=1
+
         for i in range(len(result)):
-            if result[i] != 0:
+            if result[i] !=0:
                 return False
         return True
-        
