cdb972c81ffc4984510401a40562ef389129874c
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-01-26 18:34:54 -0500

    Time: 1156 ms (46.21%) | Memory: 55.2 MB (90.97%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,9 +1,7 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-
-        res= 0 
-
+        res = 0
         prevEnd = intervals[0][1]
 
         for start,end in intervals[1:]:
@@ -14,3 +12,14 @@ class Solution:
                 prevEnd = min(prevEnd,end)
         
         return res
+
+
+
+
+
+
+
+
+
+
+
