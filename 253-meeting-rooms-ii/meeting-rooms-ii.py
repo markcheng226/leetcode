ca39168546b98368f6a7e0167d428f2bd9068b26
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-02-01 13:29:54 -0500

    Time: 84 ms (29.85%) | Memory: 21.2 MB (9.08%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+
         time = []
 
         for start,end in intervals:
             time.append((start,1))
             time.append((end,-1))
         
-        time.sort(key = lambda x:(x[0],x[1]))
+        time.sort(key=lambda x:(x[0],x[1]))
 
         count = 0
         max_count = 0
@@ -15,6 +16,4 @@ class Solution:
             count += t[1]
             max_count = max(count,max_count)
         return max_count
-
-
-
+        
