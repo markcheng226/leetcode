3363342644095d97f4e2a213276097dd8e43a1c4
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-02-01 14:22:57 -0500

    Time: 107 ms (69.07%) | Memory: 17.4 MB (66.55%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l = 0 
+        l = 0
         r = len(numbers)-1
-
+        
         while l < r:
             curSum = numbers[l] + numbers[r]
 
@@ -12,4 +12,3 @@ class Solution:
                 l +=1
             else:
                 return [l+1,r+1]
-        
