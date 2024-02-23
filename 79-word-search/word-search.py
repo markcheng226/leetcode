8073feb466efc4ea1f579235160622cd9785e676
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-02-22 20:54:29 -0500

    Time: 509 ms (95.73%) | Memory: 16.7 MB (58.18%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -6,15 +6,22 @@ class Solution:
         def dfs(r,c,i):
             if i == len(word):
                 return True
-            if (r<0 or c<0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or (r,c) in path):
+            if (r<0 or c <0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or (r,c) in path):
                 return False
+            
             path.add((r,c))
-
-            res = (dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1) or dfs(r,c+1,i+1)) 
+            res = (dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1)or dfs(r,c-1,i+1))
             path.remove((r,c))
             return res
+        
+        count = defaultdict(int, sum(map(Counter, board), Counter()))
+        if count[word[0]] > count[word[-1]]:
+            word = word[::-1]
+
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r,c,0):
                     return True
-        return False     
+        return False
+
+        
