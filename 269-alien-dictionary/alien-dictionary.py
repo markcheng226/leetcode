426e52e51ca14f2d65b7d63fc830180d50033cc0
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-02-26 14:13:37 -0500

    Time: 36 ms (77.82%) | Memory: 16.7 MB (54.82%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -1,11 +1,10 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         adj = {char:set() for word in words for char in word}
-
         for i in range(len(words)-1):
-            w1,w2 = words[i], words[i+1]
+            w1,w2 = words[i],words[i+1]
             minLen = min(len(w1),len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+            if len(w1) > len(w2) and w1[:minLen] == w2 [:minLen]:
                 return ""
             for j in range(minLen):
                 if w1[j] != w2[j]:
@@ -18,17 +17,20 @@ class Solution:
         def dfs(char):
             if char in visited:
                 return visited[char]
+            
             visited[char] = True
-
+            
             for neighChar in adj[char]:
                 if dfs(neighChar):
                     return True
             visited[char] = False
             res.append(char)
-        
+
         for char in adj:
             if dfs(char):
                 return ""
+        
         res.reverse()
         return "".join(res)
+
         
