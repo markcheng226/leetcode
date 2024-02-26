4f619bd3eccf56db2483e62ed2579b0f89ffbf6f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-02-26 14:28:03 -0500

    Time: 35 ms (81.42%) | Memory: 16.8 MB (54.82%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -3,15 +3,15 @@ class Solution:
         adj = {char:set() for word in words for char in word}
 
         for i in range(len(words)-1):
-            w1, w2 = words[i],words[i+1]
+            w1,w2 = words[i],words[i+1]
             minLen = min(len(w1),len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2 [:minLen]:
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
                 return ""
             for j in range(minLen):
                 if w1[j] != w2[j]:
                     adj[w1[j]].add(w2[j])
                     break
-        
+
         visited = {}
         res = []
 
@@ -26,10 +26,10 @@ class Solution:
             visited[char] = False
 
             res.append(char)
-        
+
         for char in adj:
             if dfs(char):
                 return ""
-        
+
         res.reverse()
-        return "".join(res)
+        return "".join(res)  
