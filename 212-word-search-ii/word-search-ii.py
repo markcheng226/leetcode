e9176d0f2ca090bf844610f8df2ee5b41db6239f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-03-01 16:38:44 -0500

    Time: 1209 ms (77.78%) | Memory: 20 MB (10.04%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,9 +1,9 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
         self.children = {}
         self.isWord = False
         self.refs = 0
-    
+
     def addWord(self,word):
         cur = self
         cur.refs +=1
@@ -13,7 +13,7 @@ class TrieNode:
             cur = cur.children[c]
             cur.refs +=1
         cur.isWord = True
-    
+
     def removeWord(self,word):
         cur = self
         cur.refs -=1
@@ -22,25 +22,25 @@ class TrieNode:
                 cur = cur.children[c]
                 cur.refs -=1
 
-
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
         for w in words:
             root.addWord(w)
         
-        ROWS, COLS = len(board),len(board[0])
+        ROWS,COLS = len(board),len(board[0])
         res,visit = set(),set()
 
         def dfs(r,c,node,word):
             if(
-                r<0 or c<0 or r == ROWS or c == COLS 
-            or (r,c) in visit 
-            or board[r][c] not in node.children 
-            or node.children[board[r][c]].refs<1
+                r not in range(ROWS) or 
+                c not in range(COLS) or 
+                (r,c) in visit or 
+                board[r][c] not in node.children or
+                node.children[board[r][c]].refs < 1
             ):
                 return
-
+            
             visit.add((r,c))
             node = node.children[board[r][c]]
             word += board[r][c]
@@ -48,13 +48,12 @@ class Solution:
                 node.isWord = False
                 res.add(word)
                 root.removeWord(word)
+
             dfs(r+1,c,node,word)
             dfs(r-1,c,node,word)
             dfs(r,c+1,node,word)
             dfs(r,c-1,node,word)
-
             visit.remove((r,c))
-
         for r in range(ROWS):
             for c in range(COLS):
                 dfs(r,c,root,"")
