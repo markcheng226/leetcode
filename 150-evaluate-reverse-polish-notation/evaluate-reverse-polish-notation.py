22ed219d3901dc7945590608257c14e06440d233
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-03-11 19:39:09 -0400

    Time: 61 ms (82.85%) | Memory: 17.2 MB (36.94%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -5,13 +5,13 @@ class Solution:
             if c == "+":
                 stack.append(stack.pop()+stack.pop())
             elif c == "-":
-                a,b = stack.pop(),stack.pop()
+                a,b = stack.pop(), stack.pop()
                 stack.append(b-a)
             elif c == "*":
-                stack.append(stack.pop()*stack.pop())
+                stack.append(stack.pop() * stack.pop())
             elif c == "/":
                 a,b = stack.pop(),stack.pop()
-                stack.append(int(b/a))
+                stack.append(int(b/a))     
             else:
                 stack.append(int(c))
-        return stack[0]
+        return stack[0]      
