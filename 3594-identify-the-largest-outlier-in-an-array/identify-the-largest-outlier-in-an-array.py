27e906239c5abd39ef879e4a83cbdcfaef4a3b4e
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2024-12-09 21:03:40 -0500

    Time: 1793 ms (13.25%) | Memory: 30.5 MB (16.02%) - LeetSync

--- 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.py
@@ -1,16 +1,18 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
-        totalsum=sum(nums)
-        counts=Counter(nums)
-        outlier=float("-inf")
+        totalsum = sum(nums)
+        counts = Counter(nums)
+        outlier = float("-inf")
 
         for num in nums:
             counts[num] -=1
-            totalsum -= num
-
-            if totalsum % 2 == 0 and counts[totalsum//2]>0:
+            totalsum -=num
+            if totalsum % 2 == 0 and counts[totalsum//2] > 0 :
                 outlier = max(outlier,num)
-            
-            counts[num] +=1
+            counts[num] += 1
             totalsum += num
         return outlier
+            
+
+        
+        
