788abc98a387f4fe93be0f9c88ccd24ec2f45f25
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-06 20:00:51 -0500

    Time: 7 ms (83.96%) | Memory: 18.3 MB (34.30%) - LeetSync

--- 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -1,22 +1,19 @@
-from collections import defaultdict
-from itertools import combinations
+
+
+
 
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> str:
-    
         G = defaultdict(list)
         for (time, user, web) in sorted(zip(timestamp, username, website)):
             G[user].append(web)
-        
         scores = defaultdict(int)
         for user, websites in G.items():
             for pattern in set(combinations(websites, 3)):
                 scores[pattern] += 1
-     
         max_pattern, max_cnt = '', 0
         for pattern, cnt in scores.items():
             if cnt > max_cnt or (cnt == max_cnt and pattern < max_pattern):
                 max_pattern = pattern
                 max_cnt = cnt
-        
         return max_pattern
