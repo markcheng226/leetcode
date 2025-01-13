744bc69d7dad708fe5336c964233e952ec8a17af
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-13 14:12:21 -0500

    Update file 1/13/2025, 2:12:21 PM

--- 680-ValidPalindromeII/680-ValidPalindromeII.py
+++ 680-ValidPalindromeII/680-ValidPalindromeII.py
@@ -1,17 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def is_palindrome(l,r):
+        def ispalindorme(l,r):
             while l < r:
                 if s[l] != s[r]:
                     return False
-                l +=1
+                l+=1
                 r-=1
             return True
         
-        l,r = 0,len(s)-1
+        l,r = 0 ,len(s)-1
         while l < r:
             if s[l] != s[r]:
-                return (is_palindrome(l+1,r) or is_palindrome(l,r-1))
-            l+=1
-            r -=1
+                return (ispalindorme(l+1,r) or ispalindorme(l,r-1))
+            l +=1
+            r-=1
         return True
+
+        
