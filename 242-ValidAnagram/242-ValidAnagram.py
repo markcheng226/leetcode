f30be1f71fdcc2ccb110dc8591c9474fdbbca959
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-13 11:04:16 -0500

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        res = [0] * 26
+
+        for i in s:
+            res[ord(i)-ord("a")] +=1
+        for i in t:
+            res[ord(i)-ord("a")] -=1
+        for i in range(len(res)):
+            if res[i] != 0:
+                return False
+        return True
