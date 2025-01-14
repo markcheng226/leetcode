14b7d1e7d8ae5362a90aacbc2048fafedc97f23f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-14 10:24:31 -0500

    Update file 1/14/2025, 10:24:31 AM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -6,25 +6,23 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
             for j in range(i+1,len(nums)):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-
-                l , r = j+1, len(nums)-1
-
+                
+                l,r = j+1,len(nums)-1
                 while l < r:
                     fourSum = nums[i] + nums[j] + nums[l] + nums[r]
                     if fourSum > target:
-                        r -=1
+                        r-=1
                     elif fourSum < target:
                         l +=1
                     else:
                         res.append([nums[i],nums[j],nums[l],nums[r]])
-                        l+=1
-                        r-=1
+                        l +=1
+                        r -=1
                         while l < r and nums[l] == nums[l-1]:
                             l +=1
                         while l < r and nums[r] == nums[r+1]:
                             r -=1
-        return res        
+        return res
