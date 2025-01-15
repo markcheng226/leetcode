075c118f1b99003281a7b4785f2d9fc15726440a
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-15 18:59:54 -0500

    Update file 1/15/2025, 6:59:54 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,6 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
+        l , r = 0, len(nums)-1
 
         while l <= r:
             m = (l+r)//2
@@ -8,7 +8,7 @@ class Solution:
             if nums[m] > target:
                 r = m-1
             elif nums[m] < target:
-                l = m+1
+                l = m +1
             else:
                 return m
-        return -1
+        return -1 
