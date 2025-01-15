dbbc674ecfd3149514e0e67c5232d66201807b9b
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-15 11:00:03 -0500

    Create file

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        low = float("inf")
+        res = 0
+
+        for i in range(len(prices)):
+            low = min(low,prices[i])
+            res = max(res,prices[i]-low)
+        return res
