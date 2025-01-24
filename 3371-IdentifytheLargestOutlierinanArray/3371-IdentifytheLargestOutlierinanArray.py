f23f62c5c3f499495647af2faa32d6ed793446dc
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-23 20:55:21 -0500

    Create file

---
+++ 3371-IdentifytheLargestOutlierinanArray/3371-IdentifytheLargestOutlierinanArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        totalsum = sum(nums)
+        count = Counter(nums)
+        res = float("-inf")
+
+        for num in nums:
+            count[num] -=1
+            totalsum -= num
+            if totalsum % 2 == 0 and count[totalsum//2] > 0 :
+                res = max(res,num)
+            count[num] +=1
+            totalsum +=num
+        return res
