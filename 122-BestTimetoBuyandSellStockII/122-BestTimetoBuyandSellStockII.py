b3a9b76f717cee8d501b3e56d0f6224888dababc
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-25 18:17:09 -0500

    Update file 1/25/2025, 6:17:09 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        next_buy = next_sell = 0
+        cur_buy = cur_sell =0
 
-        for i in range(1,len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
-        return profit
+        for i in range(len(prices)-1,-1,-1):
+            cur_buy = max(next_buy,-prices[i]+next_sell)
+            cur_sell = max(next_sell, prices[i] + next_buy)
+            next_buy = cur_buy
+            next_sell = cur_sell
+        return cur_buy
