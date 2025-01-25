3fd1e6c8a0f51d096a4e8e4b830698dfc10e7d38
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-25 11:28:46 -0500

    Update file 1/25/2025, 11:28:46 AM

--- 146-LRUCache/146-LRUCache.py
+++ 146-LRUCache/146-LRUCache.py
@@ -2,23 +2,23 @@ class Node:
     def __init__(self,key,value):
         self.key,self.value = key,value
         self.next = self.prev = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
-        self.left, self.right = Node(0,0),Node(0,0)
-        self.left.next,self.right.prev = self.right,self.left
-    
+        self.left,self.right = Node(0,0),Node(0,0)
+        self.left.next,self.right.prev=self.right,self.left   
+
     def remove(self,node):
-        prev,nxt = node.prev, node.next
-        prev.next,nxt.prev = nxt,prev
+        prev,nxt = node.prev,node.next
+        prev.next, nxt.prev = nxt,prev
 
     def add(self,node):
         prev,nxt = self.right.prev,self.right
         prev.next = nxt.prev = node
-        node.prev,node.next = prev,nxt
- 
+        node.prev,node.next = prev,nxt     
 
     def get(self, key: int) -> int:
         if key in self.cache:
