cbfa201a76198e9fa9b6dc2a748e2157bb77c211
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-25 22:56:01 -0500

    Create file

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        final = len(nums)-1
+
+        for i in range(len(nums)-2,-1,-1):
+            if i + nums[i] >= final:
+                final = i
+        if final == 0:
+            return True
+        else:
+            return False
