77d8e017c4f9bfc252608197039dbd20326e104f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-25 21:28:46 -0500

    Update file 1/25/2025, 9:28:46 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,15 +1,17 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+
         rows,cols = len(board),len(board[0])
 
         def dfs(r,c,i):
             if i == len(word):
                 return True
-            if (r<0 or c<0 or r>=rows or c>=cols or board[r][c] == "#" or word[i] != board[r][c]):
+            
+            if (r<0 or c<0 or r>=rows or c>=cols or word[i] != board[r][c] or board[r][c] == "#"):
                 return False
             
             board[r][c] = "#"
-            res=(dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1))
+            res = (dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1))
             board[r][c] = word[i]
             return res
 
