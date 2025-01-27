2a19aa4b479d6c28b562bfd19c2fd416bc16a26f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-27 11:09:30 -0500

    Update file 1/27/2025, 11:09:30 AM

--- 140-WordBreakII/140-WordBreakII.py
+++ 140-WordBreakII/140-WordBreakII.py
@@ -1,19 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        res=[]
-        cur = []
         wordDict = set(wordDict)
+        cur = []
+        res = []
 
         def backtrack(i):
             if i == len(s):
                 res.append(" ".join(cur))
                 return
-                
+            
             for j in range(i,len(s)):
-                if s[i:j+1] in wordDict:
-                    cur.append(s[i:j+1])
+                w = s[i:j+1]
+                if w in wordDict:
+                    cur.append(w)
                     backtrack(j+1)
                     cur.pop()
         backtrack(0)
         return res
-                    
