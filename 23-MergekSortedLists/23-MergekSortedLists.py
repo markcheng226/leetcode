dadc8ee0aa6c84ab86c3a8fa4a51c94d976510bf
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-27 18:34:19 -0500

    Update file 1/27/2025, 6:34:19 PM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -5,27 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        
         if len(lists) == 0:
             return None
-        
         if len(lists) == 1:
             return lists[0]
         
         while len(lists) > 1:
-            mergedLists = []
+            mergedList = []
             for i in range(0,len(lists),2):
                 l1 = lists[i]
-                l2 = lists[i+1] if i+1<len(lists) else None
-                mergedLists.append(self.mergeLists(l1,l2))
-            lists = mergedLists
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                mergedList.append(self.mergeList(l1,l2))
+            lists = mergedList
         return lists[0]
 
-        
-    
-
-    def mergeLists(self,l1,l2):
-        dommy = ListNode()
-        tail = dommy
+    def mergeList(self,l1,l2):
+        dummy = ListNode()
+        tail = dummy
         while l1 and l2:
             if l1.val < l2.val:
                 tail.next = l1
@@ -33,10 +31,10 @@ class Solution:
             else:
                 tail.next = l2
                 l2 = l2.next
-            tail=tail.next
+            tail = tail.next
+            
         if l1:
             tail.next = l1
         if l2:
             tail.next = l2
-        return dommy.next
-                
+        return dummy.next
