f2863b19527cd9e1e2d2fbc1c49b9dfd9ba5f6bd
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-27 17:27:32 -0500

    Update file 1/27/2025, 5:27:32 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -3,25 +3,24 @@ class Solution:
         resindex = 0
         reslen = 0
 
+
         for i in range(len(s)):
-            
-            l=r=i
+            l = r = i
+            while l >= 0 and r <len(s) and s[l] == s[r]:
+                if (r-l+1) > reslen:
 
-            while l >= 0 and r<len(s) and s[l] == s[r]:
-                if(r-l+1)>reslen:
                     resindex = l
-                    reslen=r-l+1
+                    reslen = r-l+1
                 l-=1
-                r +=1
-            
-            l=i
-            r=i+1
-            while l >= 0 and r<len(s) and s[l] == s[r]:
-                if(r-l+1)>reslen:
+                r+=1
+        
+            l = i
+            r = i+1
+            while l >= 0 and r <len(s) and s[l] == s[r]:
+                if (r-l+1) > reslen:
                     resindex = l
-                    reslen=r-l+1
+                    reslen = r-l+1
                 l-=1
-                r +=1
+                r+=1
         return s[resindex:resindex+reslen]
-
-            
+                
