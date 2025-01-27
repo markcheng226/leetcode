e73cefd666b76b7c128052c6459807bfd0cd2f62
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-27 12:22:02 -0500

    Update file 1/27/2025, 12:22:02 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -3,11 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = [0]*3
+        count = [0] *3
         for num in nums:
             count[num] +=1
         
         index = 0
+
         for i in range(3):
             while count[i]:
                 count[i] -=1
