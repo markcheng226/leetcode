05622c2428eb49e26cfb6bf4a2cdd80d1636ac92
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-26 21:25:50 -0500

    Update file 1/26/2025, 9:25:50 PM

--- 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.py
+++ 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
+        res = 0 
 
-        res = 0
         while True:
-            if (len(nums) != len(set(nums)) and nums):
-                res +=1
+            if len(nums) != len(set(nums)) and nums:
                 nums = nums[3:]
+                res +=1
                 continue
             break
         return res
