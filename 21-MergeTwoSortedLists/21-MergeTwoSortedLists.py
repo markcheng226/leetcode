d4dc0a6301640b2745d1d55d856331ca3673c9a9
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-28 13:37:39 -0500

    Update file 1/28/2025, 1:37:39 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -11,11 +11,11 @@ class Solution:
         while list1 and list2:
             if list1.val < list2.val:
                 tail.next = list1
-                list1 =list1.next
+                list1 = list1.next
             else:
                 tail.next = list2
                 list2 = list2.next
+            
             tail = tail.next
         tail.next = list1 or list2
-        
         return dummy.next
