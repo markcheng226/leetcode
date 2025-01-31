b66b3f7d4d217b9215c43b8c1268a975c4882135
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-31 18:29:04 -0500

    Update file 1/31/2025, 6:29:03 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,24 +1,24 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         directions = [1,0],[-1,0],[0,1],[0,-1]
-        fresh = 0
         res = 0
-        rows, cols = len(grid),len(grid[0])
+        fresh = 0
+        rows,cols = len(grid),len(grid[0])
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     fresh +=1
         
-        while fresh > 0:
+        while fresh > 0 :
             flag = False
             for r in range(rows):
                 for c in range(cols):
                     if grid[r][c] == 2:
-                        for dc, dr in directions:
-                            row,col = r+ dr, c + dc
-                            if (row in range(rows) and col in range(cols) and grid[row][col] == 1):
-                                grid[row][col] =3
+                        for dr,dc in directions:
+                            row,col=r+dr,c+dc
+                            if(row in range(rows) and col in range(cols) and grid[row][col] == 1):
+                                grid[row][col] =3 
                                 fresh -=1
                                 flag = True
             if not flag:
@@ -26,7 +26,7 @@ class Solution:
             
             for r in range(rows):
                 for c in range(cols):
-                    if grid[r][c] == 3:
-                        grid[r][c] = 2
+                    if grid[r][c] ==3:
+                        grid[r][c] =2
             res +=1
         return res
