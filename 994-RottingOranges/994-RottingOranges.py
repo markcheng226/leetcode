6d77b6ab001ec9834bcf5b53152f970bf7638596
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-30 22:42:34 -0500

    Update file 1/30/2025, 10:42:34 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -10,26 +10,23 @@ class Solution:
                 if grid[r][c] == 1:
                     fresh +=1
         
-        while fresh > 0 :
+        while fresh > 0:
             flag = False
             for r in range(rows):
                 for c in range(cols):
                     if grid[r][c] == 2:
                         for dr,dc in directions:
                             row,col = r+dr,c+dc
-                            if (row in range(rows) and col in range(cols) and grid[row][col] == 1):
-                                grid[row][col]=3
+                            if(row in range(rows) and col in range(cols) and grid[row][col] == 1):
+                                grid[row][col] =3
                                 fresh -=1
                                 flag = True
             if not flag:
                 return -1
-
+            
             for r in range(rows):
                 for c in range(cols):
                     if grid[r][c] == 3:
                         grid[r][c] =2
             res +=1
         return res
-
-
-
