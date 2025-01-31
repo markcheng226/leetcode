0c99b2015954d800b2c38efa04f3d1b23c977ae9
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-01-31 00:06:03 -0500

    Create file

---
+++ 101-SymmetricTree/101-SymmetricTree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+
+    def isMirror(self, t1: Optional[TreeNode], t2: Optional[TreeNode]) -> bool:
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+        return (t1.val == t2.val and
+                self.isMirror(t1.left, t2.right) and
+                self.isMirror(t1.right, t2.left))
