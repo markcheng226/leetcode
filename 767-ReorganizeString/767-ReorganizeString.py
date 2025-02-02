6d3073f57ea602c00fe631d681a83e9062cc9cec
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-02 11:47:08 -0500

    Update file 2/2/2025, 11:47:08 AM

--- 767-ReorganizeString/767-ReorganizeString.py
+++ 767-ReorganizeString/767-ReorganizeString.py
@@ -1,9 +1,9 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         count = Counter(s)
-        maxHeap = [[-count,char] for char,count in count.items()]
+        maxHeap = [[-count,char] for char, count in count.items()]
         heapq.heapify(maxHeap)
-
+        
         res = ""
         prev = None
 
@@ -13,7 +13,7 @@ class Solution:
             
             count,char = heapq.heappop(maxHeap)
             res += char
-            count += 1
+            count +=1
 
             if prev:
                 heapq.heappush(maxHeap,prev)
