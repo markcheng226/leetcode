2029a6b84d852b13cd7cff50659c9728141bd7fa
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-02 00:44:52 -0500

    Update file 2/2/2025, 12:44:52 AM

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,9 +1,9 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if x == 0 :
+        if x == 0:
             return 0
-
-        if n == 0:
+        
+        if n ==0:
             return 1
 
         res = 1
@@ -13,5 +13,5 @@ class Solution:
             if power %2 ==1:
                 res *= x
             x *= x
-            power = power //2
-        return res if n >=0 else 1/res
+            power = power//2
+        return res if n >= 0 else 1/res
