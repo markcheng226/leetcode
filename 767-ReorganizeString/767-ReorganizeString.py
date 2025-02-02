a177ab8a872310cb0c7a1744311d33b96296802e
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-01 22:14:11 -0500

    Create file

---
+++ 767-ReorganizeString/767-ReorganizeString.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        maxHeap = [[-count,char] for char, count in count.items()]
+        heapq.heapify(maxHeap)
+
+        res = ""
+        prev = None
+
+        while maxHeap or prev:
+            if prev and not maxHeap:
+                return ""
+            
+            count,char = heapq.heappop(maxHeap)
+            res += char
+            count +=1
+
+            if prev:
+                heapq.heappush(maxHeap,prev)
+                prev = None
+            
+            if count != 0:
+                prev = [count,char]
+        return res
