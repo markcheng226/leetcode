141ef702b2a25b4717e9b67ddeaf59ec98c0caa2
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-03 13:34:16 -0500

    Update file 2/3/2025, 1:34:16 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
@@ -12,8 +12,8 @@ class Solution:
             if l > q[0]:
                 q.popleft()
             
-            if (r+1) >=k:
+            if (r+1) >= k:
                 res.append(nums[q[0]])
                 l +=1
-            r+=1
+            r +=1
         return res
