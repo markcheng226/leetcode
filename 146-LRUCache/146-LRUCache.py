84511a718462e0bf74957793423df30ac112eced
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-02 19:53:55 -0500

    Update file 2/2/2025, 7:53:54 PM

--- 146-LRUCache/146-LRUCache.py
+++ 146-LRUCache/146-LRUCache.py
@@ -3,6 +3,7 @@ class Node:
         self.key,self.value = key,value
         self.next = self.prev = None
 
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -10,7 +11,7 @@ class LRUCache:
         self.cache = {}
         self.left,self.right = Node(0,0),Node(0,0)
         self.left.next,self.right.prev = self.right,self.left
-
+    
     def remove(self,node):
         prev,nxt = node.prev,node.next
         prev.next,nxt.prev = nxt,prev
@@ -19,7 +20,7 @@ class LRUCache:
         prev,nxt = self.right.prev,self.right
         prev.next = nxt.prev = node
         node.prev,node.next = prev,nxt
-        
+
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -34,14 +35,12 @@ class LRUCache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key,value)
         self.add(self.cache[key])
-
-        if len(self.cache)>self.cap:
+        
+        if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
         
-
-
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
