3dba9d0140ede6c62c2829f89f99479a2e8aa1fb
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-04 14:40:15 -0500

    Update file 2/4/2025, 2:40:14 PM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -5,10 +5,8 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
         if len(lists) == 0:
             return None
-        
         if len(lists) == 1:
             return lists[0]
         
@@ -16,16 +14,17 @@ class Solution:
             res = []
             for i in range(0,len(lists),2):
                 l1 = lists[i]
-                l2 = lists[i+1] if i + 1 < len(lists) else None
+                l2 = lists[i+1] if i+1<len(lists) else None
                 res.append(self.mergeLists(l1,l2))
             lists = res
-        return lists[0]        
+        return lists[0]
 
     
-    def mergeLists(selef,l1,l2):
+
+    def mergeLists(self,l1,l2):
         dummy = ListNode()
         tail = dummy
-
+        
         while l1 and l2:
             if l1.val < l2.val:
                 tail.next = l1
