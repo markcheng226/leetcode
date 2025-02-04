6740f27a148191fd7b7c64664a7d4668ab926ce2
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-04 13:17:21 -0500

    Update file 2/4/2025, 1:17:21 PM

--- 2357-MakeArrayZerobySubtractingEqualAmounts/2357-MakeArrayZerobySubtractingEqualAmounts.py
+++ 2357-MakeArrayZerobySubtractingEqualAmounts/2357-MakeArrayZerobySubtractingEqualAmounts.py
@@ -2,11 +2,12 @@ class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         nums.sort()
         cur = 0
-        res = 0
+        res =0
 
         for num in nums:
             num -= cur
-            if num >0 :
-                cur += num
+
+            if num > 0:
+                cur +=num
                 res +=1
         return res
