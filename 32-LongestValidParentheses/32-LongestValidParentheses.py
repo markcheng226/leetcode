34e4fdb14c8f46ebe714eaf6b89f841c1b69e2f8
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-04 00:18:50 -0500

    Update file 2/4/2025, 12:18:50 AM

--- 32-LongestValidParentheses/32-LongestValidParentheses.py
+++ 32-LongestValidParentheses/32-LongestValidParentheses.py
@@ -1,27 +1,25 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        res, left,right =0,0,0
+        left, right ,res =0,0,0
 
         for c in s:
             if c == "(":
                 left +=1
             else:
                 right +=1
-            if left == right:
+            if left==right:
                 res = max(res,left*2)
-            elif right>left:
+            if left<right:
                 left = right = 0
-        
         left = right = 0
-        
+
         for c in reversed(s):
             if c == ")":
                 right +=1
             else:
                 left +=1
-            
             if left == right:
                 res = max(res,left*2)
-            elif left > right:
-                left = right = 0
+            if right < left:
+                left = right =0
         return res
