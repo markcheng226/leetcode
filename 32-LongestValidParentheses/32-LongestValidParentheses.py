792447b8a29f9742f2209922e4f1ed50060f9e98
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-03 19:52:36 -0500

    Update file 2/3/2025, 7:52:35 PM

--- 32-LongestValidParentheses/32-LongestValidParentheses.py
+++ 32-LongestValidParentheses/32-LongestValidParentheses.py
@@ -1,17 +1,29 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        if not s:
-            return 0
-        n = len(s)
-        dp = [0] * n
-        max_length = 0
 
-        for i in range(1, n):
-            if s[i] == ')':
-                if s[i-1] == '(':
-                    dp[i] = (dp[i-2] if i >= 2 else 0) + 2
-                elif i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':
-                    dp[i] = dp[i-1] + (dp[i - dp[i-1] - 2] if i - dp[i-1] >= 2 else 0) + 2
-                max_length = max(max_length, dp[i])
+        left, right, max_length = 0, 0, 0
 
+ 
+        for c in s:
+            if c == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                max_length = max(max_length, left * 2)
+            elif right > left:
+                left = right = 0  
+
+        left = right = 0
+  
+        for c in reversed(s):
+            if c == ')':
+                right += 1
+            else:
+                left += 1
+            if left == right:
+                max_length = max(max_length, left * 2)
+            elif left > right:
+                left = right = 0 
+                
         return max_length
