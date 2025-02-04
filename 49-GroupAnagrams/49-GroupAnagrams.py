28d9daa3cd6acf08d83b844234d1d97bf78f9c0f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-04 14:41:41 -0500

    Update file 2/4/2025, 2:41:41 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -3,7 +3,7 @@ class Solution:
         res = defaultdict(list)
 
         for s in strs:
-            count = [0]*26
+            count = [0] * 26
             for c in s:
                 count[ord(c)-ord("a")] +=1
             res[tuple(count)].append(s)
