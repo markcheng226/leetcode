34fc5d0b023080567f5c7d8b8ac90203adedab15
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-03 23:54:21 -0500

    Update file 2/3/2025, 11:54:20 PM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -3,27 +3,28 @@ class Solution:
         nums.sort()
         res = []
 
-        for i in range(len(nums)):
-            if i>0 and nums[i] == nums[i-1]:
+        for i in range(len(nums)):        
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i+1,len(nums)):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                
-                l, r = j+1,len(nums)-1
+            
+                l,r = j+1,len(nums)-1
 
                 while l < r:
-                    fourSum = nums[i] + nums[j] + nums[l] + nums[r]
-                    if fourSum > target:
+                    threeSum = nums[i] + +nums[j] + nums[l] + nums[r]
+                        
+                    if threeSum > target:
                         r -=1
-                    elif fourSum < target:
+                    elif threeSum < target:
                         l +=1
                     else:
                         res.append([nums[i],nums[j],nums[l],nums[r]])
-                        l +=1
-                        r -=1
-                        while l < r and nums[l] == nums[l-1]:
-                            l +=1
+                        l+=1
+                        r-=1
+                        while l<r and nums[l] == nums[l-1]:
+                            l+=1
                         while l < r and nums[r] == nums[r+1]:
-                            r-=1
+                            r -=1
         return res
