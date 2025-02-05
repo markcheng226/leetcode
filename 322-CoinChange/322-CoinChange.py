61fd2e61fd61cded640644b7f27bd4b3146cc557
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-05 15:30:19 -0500

    Update file 2/5/2025, 3:30:19 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -3,8 +3,8 @@ class Solution:
         dp = [amount+1] * (amount+1)
         dp[0] = 0
 
-        for a in range(1,amount +1):
+        for amount in range(1,amount +1):
             for c in coins:
-                if a-c>=0:
-                    dp[a] = min(dp[a],1+dp[a-c])
+                if amount-c>=0:
+                    dp[amount] = min(dp[amount],1+dp[amount-c])
         return dp[amount] if dp[amount ]!= amount+1 else -1
