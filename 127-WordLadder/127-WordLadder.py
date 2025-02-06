84555b6e3f1f61820ca41571063cfa5fbc3af37e
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-06 13:39:19 -0500

    Update file 2/6/2025, 1:39:19 PM

--- 127-WordLadder/127-WordLadder.py
+++ 127-WordLadder/127-WordLadder.py
@@ -1,25 +1,25 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        res = 0
         words = set(wordList)
-        res =0
 
         if (beginWord == endWord) or (endWord not in wordList):
             return 0
         
         q = deque([beginWord])
-            
+
         while q:
             res +=1
             for i in range(len(q)):
                 node = q.popleft()
                 if node == endWord:
-                    return res
+                    return res     
                 for i in range(len(node)):
                     for c in range(97,123):
                         if chr(c) == node[i]:
                             continue
                         nei = node[:i] + chr(c) + node[i+1:]
                         if nei in words:
-                            q.append(nei)
                             words.remove(nei)
+                            q.append(nei)
         return 0
