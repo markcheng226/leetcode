67523e169444f425471c7e87baae694350dff727
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-06 19:59:43 -0500

    Update file 2/6/2025, 7:59:43 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.py
@@ -15,9 +15,7 @@ class Solution:
                 res.append([])
             
             res[depth].append(node.val)
-            dfs(node.left,depth +1)
-            dfs(node.right,depth +1)
-
+            dfs(node.left,depth+1)
+            dfs(node.right,depth+1)
         dfs(root,0)
         return res
-            
