d94febf435269efce422714234d4d29992a45da0
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-08 14:23:31 -0500

    File updated at 2/8/2025, 2:23:31 PM

--- 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.py
+++ 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.py
@@ -1,17 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head is None:
             return None
-            
+        
         l1 = head
         while l1 is not None:
             l2 = Node(l1.val)
             l2.next = l1.next
             l1.next = l2
             l1 = l2.next
-        newhead=head.next
-
+        
+        newhead = head.next
         l1 = head
         while l1 is not None:
             if l1.random is not None:
