cceba6315bf6baedd9215494c857ce948ec99312
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-08 17:23:24 -0500

    File updated at 2/8/2025, 5:23:24 PM

--- 198-HouseRobber/198-HouseRobber.py
+++ 198-HouseRobber/198-HouseRobber.py
@@ -1,9 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1 , rob2 = 0, 0 
-
+        rob1,rob2 = 0,0
         for i in range(len(nums)):
-            rob3 = max(nums[i] + rob1,rob2)
-            rob1 = rob2
-            rob2 = rob3
+            rob3 = max(nums[i]+rob1,rob2)
+            rob1 =rob2
+            rob2 =rob3
         return rob2
