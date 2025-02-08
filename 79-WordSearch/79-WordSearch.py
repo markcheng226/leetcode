78122e8a66aba251301421f526b353a7e3562884
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-08 17:11:27 -0500

    File updated at 2/8/2025, 5:11:27 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -6,7 +6,7 @@ class Solution:
             if i == len(word):
                 return True
             
-            if(r<0 or c<0 or r>=rows or c>= cols or board[r][c] == "#" or word[i] != board[r][c]):
+            if (r<0 or c<0 or r>= rows or c>=cols or board[r][c] != word[i] or board[r][c] == "#"):
                 return False
             
             board[r][c] = "#"
@@ -19,3 +19,4 @@ class Solution:
                 if dfs(r,c,0):
                     return True
         return False
+                
