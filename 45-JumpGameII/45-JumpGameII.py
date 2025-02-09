faf395f9a703a89e6b2319ac7456da556a8ada4c
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-08 22:52:42 -0500

    File updated at 2/8/2025, 10:52:41 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -4,10 +4,10 @@ class Solution:
         r = l = 0
 
         while r < len(nums)-1:
-            farest = 0
+            far = 0
             for i in range(l,r+1):
-                farest = max(farest,nums[i]+i)
+                far = max(far,nums[i]+i)
             l = r+1
-            r = farest
+            r = far
             res +=1
         return res
