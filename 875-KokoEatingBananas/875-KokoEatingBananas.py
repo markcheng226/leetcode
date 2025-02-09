11061fc825feb18a0ae86e1ec222db2389d443cc
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-09 16:02:06 -0500

    File updated at 2/9/2025, 4:02:06 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,10 +1,11 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l,r = 1,max(piles)
+
         res = r
 
         while l <= r:
-            k = (l+r)//2
+            k = (l+r) //2
             hours = sum(math.ceil(p/k) for p in piles)
 
             if hours <= h:
@@ -13,5 +14,3 @@ class Solution:
             else:
                 l = k+1
         return res
-
-            
