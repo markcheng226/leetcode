ed6903bd04cfbf9f222a85d1caa703aab4162baa
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-09 11:15:53 -0500

    File updated at 2/9/2025, 11:15:53 AM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
@@ -2,7 +2,8 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
         l = r =0
-        q= deque()
+        q = deque()
+
 
         while r < len(nums):
             while q and nums[q[-1]] < nums[r]:
@@ -11,8 +12,8 @@ class Solution:
 
             if l > q[0]:
                 q.popleft()
-
-            if (r-l+1) >= k:
+            
+            if (r-l+1)>=k:
                 res.append(nums[q[0]])
                 l+=1
             r+=1
