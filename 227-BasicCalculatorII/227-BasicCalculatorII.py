6967ae5320a1bd6cd93af69c74d80ca2b8ecb6ac
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-10 12:15:49 -0500

    File updated at 2/10/2025, 12:15:49 PM

--- 227-BasicCalculatorII/227-BasicCalculatorII.py
+++ 227-BasicCalculatorII/227-BasicCalculatorII.py
@@ -1,14 +1,13 @@
 class Solution:
     def calculate(self, s: str) -> int:
         stack = []
-        sign = '+'
+        sign ='+'
         num = 0
 
         for i in range(len(s)):
             char = s[i]
             if char.isdigit():
-                num = num *10 + int(char)
-
+                num = num*10 + int(char)
             if char in "+-*/" or i == len(s)-1:
                 if sign == '+':
                     stack.append(num)
@@ -17,7 +16,7 @@ class Solution:
                 elif sign == '*':
                     stack.append(stack.pop() * num)
                 elif sign == '/':
-                    stack.append(int(stack.pop()  /num))
+                    stack.append(int(stack.pop() / num))
                 sign = char
                 num = 0
         return sum(stack)
