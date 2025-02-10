822715ccbbcab0deb0644943e7b6c1c34ccd5391
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-09 19:40:07 -0500

    File updated at 2/9/2025, 7:40:07 PM

--- 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
+++ 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
@@ -3,11 +3,12 @@ class Solution:
         data = sorted(zip(timestamp,username,website))
         users = defaultdict(list)
 
-        for i,user,site in data:
+        for i, user,site in data:
             users[user].append(site)
         
         pattern = Counter()
 
         for user,sites in users.items():
             pattern.update(set(combinations(sites,3)))
+        
         return min(pattern,key = lambda x:(-pattern[x],x))
