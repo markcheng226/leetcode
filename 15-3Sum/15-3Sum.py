b55557704b7db86511152179b30cedd008dfc938
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-10 16:08:16 -0500

    File updated at 2/10/2025, 4:08:15 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -10,18 +10,18 @@ class Solution:
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            l,r = i+1,len(nums)-1
+            l , r = i+1,len(nums)-1
 
             while l < r:
                 threeSum = nums[i] + nums[l] + nums[r]
-                if threeSum < 0:
-                    l +=1
-                elif threeSum > 0:
+                if threeSum > 0:
                     r -=1
+                elif threeSum < 0:
+                    l +=1
                 else:
                     res.append([nums[i],nums[l],nums[r]])
-                    l +=1
-                    r -=1
+                    l+=1
+                    r-=1
                     while l < r and nums[l] == nums[l-1]:
                         l +=1
         return res
