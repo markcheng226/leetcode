89ed9f4bacb903efd574640509a8e87c841ea36f
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-09 22:39:12 -0500

    File updated at 2/9/2025, 10:39:12 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.py
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.py
@@ -3,9 +3,11 @@ class Solution:
         merge = nums1+nums2
         merge.sort()
         total = len(merge)
-        if total % 2 ==1:
+
+        if total %2 ==1:
             return float(merge[total//2])
+        
         else:
-            merge1 = merge[total//2-1]
+            merge1= merge[total//2-1]
             merge2 = merge[total//2]
-            return float((merge1+merge2)/2)
+            return float(merge1+merge2)/2
