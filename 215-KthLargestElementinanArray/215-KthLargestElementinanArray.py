d9b3f7041834c71e63302c17be746997ac368088
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-10 12:20:32 -0500

    File updated at 2/10/2025, 12:20:32 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
@@ -3,6 +3,6 @@ class Solution:
         min_heap = []
         for num in nums:
             heapq.heappush(min_heap,num)
-            if len(min_heap)>k:
+            if len(min_heap) > k:
                 heapq.heappop(min_heap)
         return min_heap[0]
