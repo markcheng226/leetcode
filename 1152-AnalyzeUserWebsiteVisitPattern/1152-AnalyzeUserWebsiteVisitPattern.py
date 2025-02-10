9d1ae011ca949ff5382e4e89f60b79aa783c420e
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-09 19:18:21 -0500

    File updated at 2/9/2025, 7:18:21 PM

--- 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
+++ 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
@@ -1,30 +1,14 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        
-        G = defaultdict(list)
-        for time, user, website in sorted(zip(timestamp, username, website)):
-            G[user].append(website)
-        
+        data = sorted(zip(timestamp,username,website))
+        users = defaultdict(list)
 
-        scores = defaultdict(int)
-        for users, websites in G.items():
-            size = len(websites)
-            pattern = set()
-            
+        for i ,user,site in data:
+            users[user].append(site)
+        
+        patterns = Counter()
 
-            for i in range(size-2):
-                for j in range(i+1, size-1):
-                    for k in range(j+1,size):
-                        curr_pattern = (websites[i], websites[j], websites[k])
-                        if curr_pattern not in pattern:
-                            pattern.add(curr_pattern)
-                            scores[curr_pattern] +=1
+        for user,sites in users.items():
+            patterns.update(set(combinations(sites,3)))
         
-        
-        max_pattern, max_score = '', 0
-        for pattern, count in scores.items():
-            if count > max_score or (count == max_score and pattern < max_pattern):
-                max_pattern = pattern
-                max_score = count
-        
-        return max_pattern
+        return min(patterns,key = lambda x:(-patterns[x],x))
