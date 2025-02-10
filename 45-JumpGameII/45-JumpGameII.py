3e6f7daa3fe6ccda1a5867244379af640fd75b54
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-10 15:50:34 -0500

    File updated at 2/10/2025, 3:50:34 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,13 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l = r = 0
+        l =r =0 
         res = 0
 
         while r < len(nums)-1:
             farest = 0
             for i in range(l,r+1):
                 farest = max(farest,nums[i]+i)
-            l = r+1
+            l = r
             r = farest
             res +=1
         return res
