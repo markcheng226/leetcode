543a89c7ce221a5f135accb93434667e01ecb2f7
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-11 15:57:18 -0500

    File updated at 2/11/2025, 3:57:18 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.py
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.py
@@ -2,13 +2,13 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         merge = nums1+nums2
         merge.sort()
+
         total = len(merge)
 
-        if total %2 ==1:
+        if total % 2 ==1:
             return float(merge[total//2])
-            
+    
         else:
-            merge1 = merge[total//2-1]
-            merge2 = merge[total//2]
+            merge1 = merge[total//2]
+            merge2 = merge[total//2-1]
             return float(merge1+merge2)/2
-        
