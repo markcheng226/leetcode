ba4fd5f443211e2d71c17d5f98dbfc2aca72ca93
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-11 10:04:56 -0500

    File updated at 2/11/2025, 10:04:56 AM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,18 +1,21 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
-        prefixSum = {0:1}
         curSum = 0
+        prefixSum = {0:1}
 
         for num in nums:
-            curSum +=num
+            curSum += num
             diff = curSum -k
-
+        
             if diff in prefixSum:
                 res += prefixSum[diff]
             
             if curSum in prefixSum:
                 prefixSum[curSum] +=1
             else:
-                prefixSum[curSum] = 1
+                prefixSum[curSum] =1
         return res
+
+        
+        
