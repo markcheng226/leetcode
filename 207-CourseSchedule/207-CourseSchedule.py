5ea8177b69826b2b33ee342735a282539dac1071
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-11 08:26:13 -0500

    File updated at 2/11/2025, 8:26:12 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -18,10 +18,10 @@ class Solution:
                 if not dfs(pre):
                     return False
             visiting.remove(crs)
-            preMap[crs] =[]
+            preMap[crs] = []
             return True
+        
         for c in range(numCourses):
             if not dfs(c):
                 return False
         return True
-                    
