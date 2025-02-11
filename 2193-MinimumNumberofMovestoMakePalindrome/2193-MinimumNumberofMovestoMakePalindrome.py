01659d262cf129194c3d5e01b9a003da38f3397b
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-11 15:20:36 -0500

    File updated at 2/11/2025, 3:20:36 PM

--- 2193-MinimumNumberofMovestoMakePalindrome/2193-MinimumNumberofMovestoMakePalindrome.py
+++ 2193-MinimumNumberofMovestoMakePalindrome/2193-MinimumNumberofMovestoMakePalindrome.py
@@ -2,13 +2,12 @@ class Solution:
     def minMovesToMakePalindrome(self, s: str) -> int:
         s = list(s)
         res = 0
-
-        while len(s) >1:
+        while len(s) > 1:
             i = s.index(s[-1])
             if i == len(s)-1:
                 res += i//2
             else:
-                res += i
+                res+=i
                 s.pop(i)
             s.pop()
         return res
