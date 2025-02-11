2c18bc3312d66492cf1975a0443da6077073f313
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-11 17:13:34 -0500

    File updated at 2/11/2025, 5:13:34 PM

--- 451-SortCharactersByFrequency/451-SortCharactersByFrequency.py
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.py
@@ -1,11 +1,11 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        count = Counter(s)
-        maxHeap = [[-count,char] for char, count in count.items()]
+        count =Counter(s)
+        maxHeap = [[-count,char] for char,count in count.items()]
         heapq.heapify(maxHeap)
         res = []
 
         while maxHeap:
             count,char = heapq.heappop(maxHeap)
-            res.append(char*-count)
+            res.append(-count*char)
         return "".join(res)
