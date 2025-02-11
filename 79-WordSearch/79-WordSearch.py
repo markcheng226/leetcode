fe4a992aa55c35158ec8260288148e7b26b1a1d8
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-11 10:22:30 -0500

    File updated at 2/11/2025, 10:22:30 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -5,14 +5,15 @@ class Solution:
         def dfs(r,c,i):
             if i == len(word):
                 return True
-            if(r<0 or c<0 or r>= rows or c>=cols or word[i] != board[r][c] or board[r][c] == "#"):
+            
+            if(r<0 or c<0 or r>=rows or c >=cols or board[r][c]!= word[i] or board[r][c] == "#"):
                 return False
             
             board[r][c] = "#"
             res = (dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1))
             board[r][c] = word[i]
             return res
-        
+
         for r in range(rows):
             for c in range(cols):
                 if dfs(r,c,0):
