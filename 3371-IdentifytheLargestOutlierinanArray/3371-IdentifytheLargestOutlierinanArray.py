0e474d20f34ff5ff89a8cbb3b64b0cafa164adb3
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 12:18:27 -0500

    File updated at 2/12/2025, 12:18:26 PM

--- 3371-IdentifytheLargestOutlierinanArray/3371-IdentifytheLargestOutlierinanArray.py
+++ 3371-IdentifytheLargestOutlierinanArray/3371-IdentifytheLargestOutlierinanArray.py
@@ -7,8 +7,9 @@ class Solution:
         for num in nums:
             count[num] -=1
             totalsum -= num
-            if totalsum %2 == 0 and count[totalsum//2]>0:
+            if totalsum %2 == 0 and count[totalsum//2] >0:
                 outlier = max(outlier,num)
+            
             count[num] +=1
-            totalsum +=num
+            totalsum += num
         return outlier
