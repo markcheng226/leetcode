5df38cb05dcf5b57c9ae0a11c40bf0432ce953ac
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 12:38:33 -0500

    File updated at 2/12/2025, 12:38:33 PM

--- 127-WordLadder/127-WordLadder.py
+++ 127-WordLadder/127-WordLadder.py
@@ -3,17 +3,18 @@ class Solution:
         res = 0
         words = set(wordList)
 
-        if (beginWord == endWord) or (endWord not in wordList):
-            return 0
-
         q = deque([beginWord])
 
+        if beginWord == endWord or endWord not in wordList:
+            return 0
+        
         while q:
             res +=1
             for i in range(len(q)):
                 node = q.popleft()
                 if node == endWord:
                     return res
+                
                 for i in range(len(node)):
                     for c in range(97,123):
                         if chr(c) == node[i]:
@@ -23,4 +24,3 @@ class Solution:
                             words.remove(nei)
                             q.append(nei)
         return 0
-            
