025d9497905c1d55975baa929a37f2a04b9d3331
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 12:18:57 -0500

    File updated at 2/12/2025, 12:18:57 PM

--- 2214-MinimumHealthtoBeatGame/2214-MinimumHealthtoBeatGame.py
+++ 2214-MinimumHealthtoBeatGame/2214-MinimumHealthtoBeatGame.py
@@ -1,3 +1,4 @@
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
-        return sum(damage)-min(max(damage),armor) +1
+        return sum(damage) - min(max(damage),armor) +1
+        
