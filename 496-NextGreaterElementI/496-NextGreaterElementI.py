e42446c05f23f6a3fcbccdf76c75b23ac603665a
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 18:39:05 -0500

    File updated at 2/12/2025, 6:39:05 PM

--- 496-NextGreaterElementI/496-NextGreaterElementI.py
+++ 496-NextGreaterElementI/496-NextGreaterElementI.py
@@ -2,8 +2,9 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
         res = {}
+
         for i in range(len(nums2)-1,-1,-1):
-            while stack and stack[-1] < nums2[i]:
+            while stack and stack[-1]< nums2[i]:
                 stack.pop()
             res[nums2[i]] = stack[-1] if stack else -1
             stack.append(nums2[i])
