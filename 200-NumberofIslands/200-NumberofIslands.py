7d82c5dc6578148e0b3bff7a79dc13d9c344f4c5
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 11:13:12 -0500

    File updated at 2/12/2025, 11:13:11 AM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,19 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         directions = [1,0],[-1,0],[0,1],[0,-1]
-        res = 0
         rows,cols = len(grid),len(grid[0])
+        res = 0
 
         def bfs(r,c):
-            q=deque()
+            q = deque()
             grid[r][c] = "0"
             q.append((r,c))
-
             while q:
                 row,col = q.popleft()
                 for dr,dc in directions:
-                    nr,nc = dr+row,dc+col
-                    if(nr<0 or nc<0 or nr>= rows or nc>=cols or grid[nr][nc] == "0"):
+                    nr,nc = row+dr,col+dc
+                    if (nr<0 or nc<0 or nr>= rows or nc>=cols or grid[nr][nc] == "0"):
                         continue
                     grid[nr][nc] = "0"
                     q.append((nr,nc))
