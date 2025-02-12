a4337cf8251b9025cdfbad86b49fd07db2961587
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 18:52:59 -0500

    File updated at 2/12/2025, 6:52:59 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -8,9 +8,12 @@ class Solution:
             for c in range(9):
                 if board[r][c] == ".":
                     continue
+                
                 if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3,c//3)]):
                     return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                squares[(r//3,c//3)].add(board[r][c])
+                
+                else:
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    squares[(r//3,c//3)].add(board[r][c])
         return True
