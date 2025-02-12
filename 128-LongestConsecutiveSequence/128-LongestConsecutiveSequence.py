09422034096cc12dbaeb6d8d715ee5c6539edcb7
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 16:25:01 -0500

    File updated at 2/12/2025, 4:25:01 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -5,7 +5,7 @@ class Solution:
 
         for num in numSet:
             if num -1 not in numSet:
-                length = 1
+                length =1
                 while num + length in numSet:
                     length +=1
                 res = max(res,length)
