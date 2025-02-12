2045603837ac0d073472bafaa9c1dd1ebaade5db
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 12:56:20 -0500

    File updated at 2/12/2025, 12:56:20 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -4,7 +4,7 @@ class Solution:
         res = 0
         directions = [1,0],[-1,0],[0,1],[0,-1]
         rows,cols = len(grid),len(grid[0])
-        
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
@@ -17,15 +17,16 @@ class Solution:
                     if grid[r][c] == 2:
                         for dr,dc in directions:
                             row,col = r+dr,c+dc
-                            if(row in range(rows) and col in range(cols) and grid[row][col] == 1):
-                                grid[row][col] = 3
+                            if (row in range(rows) and col in range(cols) and grid[row][col] == 1):
+                                grid[row][col] =3
                                 flag = True
                                 fresh -=1
             if not flag:
                 return -1
+            
             for r in range(rows):
                 for c in range(cols):
-                    if grid[r][c] ==3:
+                    if grid[r][c] == 3:
                         grid[r][c] =2
             res +=1
         return res
