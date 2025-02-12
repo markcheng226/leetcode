418115c5eab9fbd3849f17eb9275e19405f7e192
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 17:06:52 -0500

    File updated at 2/12/2025, 5:06:52 PM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -4,17 +4,21 @@ class Solution:
         stack = []
 
         def backtrack(openN,closeN):
-            if openN == closeN == n:
+            if openN==closeN==n:
                 res.append("".join(stack))
+                return res
             
             if openN < n:
                 stack.append("(")
                 backtrack(openN+1,closeN)
                 stack.pop()
-
-            if closeN < openN:
+            
+            if closeN<openN:
                 stack.append(")")
                 backtrack(openN,closeN+1)
                 stack.pop()
         backtrack(0,0)
         return res
+
+
+        
