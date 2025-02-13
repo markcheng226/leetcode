a9d586d1462fb1a0109d6663a6193ce18e415c35
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 22:48:02 -0500

    File updated at 2/12/2025, 10:48:02 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,20 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res=[]
+        res = []
         candidates.sort()
 
-        def dfs(i,cur,total):
+        def backtrack(i,cur,total):
             if total == target:
                 res.append(cur.copy())
                 return res
-
+            
             for j in range(i,len(candidates)):
-                if total +  candidates[j] > target:
+                if total + candidates[j] > target:
                     return
-                else:
-                    cur.append(candidates[j])
-                    dfs(j,cur,total+candidates[j])
-                    cur.pop()
-        dfs(0,[],0)
+                
+                cur.append(candidates[j])
+                backtrack(j,cur,total +candidates[j])
+                cur.pop()
+        backtrack(0,[],0)
         return res
-
