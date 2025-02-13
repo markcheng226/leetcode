3735b498fa4bbe4fd00c1f606c13b14e2dd32a88
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 22:50:12 -0500

    File updated at 2/12/2025, 10:50:12 PM

--- 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
+++ 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
@@ -1,12 +1,10 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         data = sorted(zip(timestamp,username,website))
-
         users = defaultdict(list)
 
-        for i,user,site in data:
+        for i, user,site in data:
             users[user].append(site)
-        
         pattern = Counter()
 
         for user,sites in users.items():
