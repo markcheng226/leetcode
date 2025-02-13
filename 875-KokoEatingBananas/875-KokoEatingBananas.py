427a7906dbc857a5024c45f26616e224bdeb32d5
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 22:40:49 -0500

    File updated at 2/12/2025, 10:40:49 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -3,10 +3,10 @@ class Solution:
         l,r = 1,max(piles)
         res = r
 
-        while l <= r:
+        while l <= r :
             k = (l+r)//2
             hours = sum(math.ceil(p/k) for p in piles)
-            
+
             if hours <= h:
                 res = min(res,k)
                 r = k-1
