dddef719e7f662bb8c623fad5cadaa4594fe1b40
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-13 10:27:00 -0500

    File updated at 2/13/2025, 10:27:00 AM

--- 443-StringCompression/443-StringCompression.py
+++ 443-StringCompression/443-StringCompression.py
@@ -1,19 +1,19 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        write = 0
-        read = 0
+        read =0
+        write =0
 
-        while read < len(chars):
+        while read<len(chars):
             char = chars[read]
-            count = 0
+            count =0
             
-            while read < len(chars) and chars[read] == char:
-                read +=1
+            while read <len(chars) and chars[read] == char:
+                read+=1
                 count +=1
             
             chars[write] = char
-            write+=1
-            
+            write +=1
+
             if count >1:
                 for digit in str(count):
                     chars[write] = digit
