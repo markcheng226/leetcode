5eb081c70f1d7469136b4aa2c33f249326994e99
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-02-12 23:28:23 -0500

    File updated at 2/12/2025, 11:28:23 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.py
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.py
@@ -5,13 +5,14 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        vals1=[]
-        while head:
-            vals1.append(head.val)
-            head = head.next
-        vals2 = list(reversed(vals1))
+        val1 = []
 
-        if vals1 == vals2:
+        while head:
+            val1.append(head.val)
+            head = head.next
+        
+        val2 = list(reversed(val1))
+        if val1 == val2:
             return True
         else:
             return False
