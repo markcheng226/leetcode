0dceb948759fa4ac21433a3b926980d1bd894ae9
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-15 00:19:08 -0400

    File updated at 3/15/2025, 12:19:08 AM

--- 3462-MaximumSumWithatMostKElements/3462-MaximumSumWithatMostKElements.py
+++ 3462-MaximumSumWithatMostKElements/3462-MaximumSumWithatMostKElements.py
@@ -4,12 +4,12 @@ class Solution:
 
         for i in range(len(grid)):
             row = sorted(grid[i],reverse = True)
-            top = (row[:limits[i]])
+            top = row[:limits[i]]
             for val in top:
                 heapq.heappush(maxHeap,-val)
-    
-    
+            
         res = 0
         for i in range(k):
             res += -heapq.heappop(maxHeap)
         return res
+        
