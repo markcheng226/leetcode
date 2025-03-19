575462d54fe6c24e035542efb2c03da6167c372d
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-19 19:41:19 -0400

    File updated at 3/19/2025, 7:41:19 PM

--- 3462-MaximumSumWithatMostKElements/3462-MaximumSumWithatMostKElements.py
+++ 3462-MaximumSumWithatMostKElements/3462-MaximumSumWithatMostKElements.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
+        maxHeap=[]
 
-
-        maxHeap = []
         for i in range(len(grid)):
             sortedRow = sorted(grid[i],reverse = True)
             topValue = sortedRow[:limits[i]]
-
+            
             for val in topValue:
                 heapq.heappush(maxHeap,-val)
+            
         res = 0
         for i in range(k):
             res += -heapq.heappop(maxHeap)
