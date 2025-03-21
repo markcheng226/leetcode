b9864f1d9a2a4831662aa72a398704507482d403
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-21 19:25:04 -0400

    File updated at 3/21/2025, 7:25:03 PM

--- 210-CourseScheduleII/210-CourseScheduleII.py
+++ 210-CourseScheduleII/210-CourseScheduleII.py
@@ -1,9 +1,9 @@
+// Last updated: 3/21/2025, 7:25:03 PM
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         preMap = {i:[] for i in range(numCourses)}
         for crs,pre in prerequisites:
             preMap[crs].append(pre)
-        
         cycle = set()
         visit = set()
         res = []
@@ -11,8 +11,10 @@ class Solution:
         def dfs(crs):
             if crs in cycle:
                 return False
+
             if crs in visit:
                 return True
+
             cycle.add(crs)
             for pre in preMap[crs]:
                 if dfs(pre) == False:
@@ -21,6 +23,7 @@ class Solution:
             visit.add(crs)
             res.append(crs)
             return True
+        
         for c in range(numCourses):
             if dfs(c) == False:
                 return []
