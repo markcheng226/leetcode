0d2bcf4bd2888cbb16ff106a1eee6a8c7803610f
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-22 09:10:21 -0400

    File updated at 3/22/2025, 9:10:21 AM

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
@@ -1,9 +1,10 @@
-// Last updated: 3/22/2025, 9:05:20 AM
+// Last updated: 3/22/2025, 9:10:21 AM
 class MedianFinder:
+
     def __init__(self):
         self.small = []
         self.large = []
-
+        
     def addNum(self, num: int) -> None:
         heapq.heappush(self.small,-num)
         heapq.heappush(self.large,-heapq.heappop(self.small))
@@ -11,12 +12,12 @@ class MedianFinder:
         if len(self.small) < len(self.large):
             heapq.heappush(self.small,-heapq.heappop(self.large))
         
+
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return -self.small[0]
         else:
-            return (-self.small[0] + self.large[0])/2
-        
+            return (-self.small[0] + self.large[0])/2        
 
 
 # Your MedianFinder object will be instantiated and called as such:
