af535b9aa019c1aea0f61c03a107606c2d2ba93f
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-24 12:32:50 -0400

    File updated at 3/24/2025, 12:32:50 PM

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,11 +1,11 @@
-# Last updated: 3/23/2025, 10:11:23 PM
+# Last updated: 3/24/2025, 12:32:50 PM
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         res = []
 
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] ==nums[i-1]:
                 continue
             for j in range(i+1,len(nums)):
                 if j > i+1 and nums[j] == nums[j-1]:
@@ -15,13 +15,13 @@ class Solution:
                 while l < r:
                     fourSum = nums[i] + nums[j] + nums[l] + nums[r]
                     if fourSum > target:
-                        r -=1
+                        r-=1
                     elif fourSum < target:
                         l +=1
                     else:
                         res.append([nums[i],nums[j],nums[l],nums[r]])
-                        r -=1
-                        l +=1
+                        l+=1
+                        r-=1
                         while l < r and nums[l] == nums[l-1]:
                             l +=1
                         while l < r and nums[r] == nums[r+1]:
