80976594898f24898aa5770e218e641308062598
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-25 20:55:52 -0400

    File updated at 3/25/2025, 8:55:51 PM

--- 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
+++ 1152-AnalyzeUserWebsiteVisitPattern/1152-AnalyzeUserWebsiteVisitPattern.py
@@ -1,14 +1,14 @@
-# Last updated: 3/24/2025, 6:08:17 PM
+# Last updated: 3/25/2025, 8:55:51 PM
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         data = sorted(zip(timestamp,username,website))
 
         users = defaultdict(list)
-        for i, user,website in data:
+        for i,user,website in data:
             users[user].append(website)
         
         pattern = Counter()
         for user,sites in users.items():
             pattern.update(set(combinations(sites,3)))
         
-        return min(pattern,key=lambda x:(-pattern[x],x))
+        return min(pattern, key=lambda x:(-pattern[x],x))
