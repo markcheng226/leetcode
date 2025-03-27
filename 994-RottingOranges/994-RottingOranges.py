359aeff5c9b95f381c7cee070acb1cab592854d8
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-03-27 18:25:23 -0400

    File updated at 3/27/2025, 6:25:23 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,10 +1,10 @@
-# Last updated: 3/26/2025, 6:44:46 PM
+# Last updated: 3/27/2025, 6:25:23 PM
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows,cols = len(grid),len(grid[0])
-        directions = [1,0],[-1,0],[0,1],[0,-1]
         res = 0
         fresh = 0
+        directions = [1,0],[-1,0],[0,1],[0,-1]
 
         for r in range(rows):
             for c in range(cols):
@@ -19,9 +19,10 @@ class Solution:
                         for dr,dc in directions:
                             row,col = r+dr,c+dc
                             if (row in range(rows) and col in range(cols) and grid[row][col] == 1):
-                                grid[row][col] = 3
+                                grid[row][col] =3
                                 flag = True
                                 fresh -=1
+            
             if not flag:
                 return -1
             
