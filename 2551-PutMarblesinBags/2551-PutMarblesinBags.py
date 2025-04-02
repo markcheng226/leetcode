7fa8f60406666c891613af566e7e6823509c4d60
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-04-02 10:06:28 -0400

    File updated at 4/2/2025, 10:06:28 AM

--- 2551-PutMarblesinBags/2551-PutMarblesinBags.py
+++ 2551-PutMarblesinBags/2551-PutMarblesinBags.py
@@ -1,17 +1,17 @@
-# Last updated: 4/1/2025, 11:54:41 AM
+# Last updated: 4/2/2025, 10:06:28 AM
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
         if k == 1:
             return 0
-            
+
         n = len(weights)
 
-        pair_cost = [weights[i]+weights[i+1] for i in range(n-1)]
+        pair_cost = [ weights[i] + weights[i+1] for i in range(n-1)]
 
         pair_cost.sort()
 
         max_cost = sum(pair_cost[-(k-1):])
 
-        min_cost = sum(pair_cost[:(k-1)])
+        min_cost = sum(pair_cost[:k-1])
 
-        return max_cost - min_cost
+        return max_cost-min_cost
