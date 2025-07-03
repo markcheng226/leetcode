557f2574bedcedbfbc15f98f47df610b380c3b45
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-03 17:39:38 -0400

    File updated at 7/3/2025, 5:39:38 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.py
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.py
@@ -1,19 +1,20 @@
-# Last updated: 7/3/2025, 5:32:30 PM
+# Last updated: 7/3/2025, 5:39:38 PM
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         maxn = curn = 0
+        vowel = "aeiou"
 
         for i in range(len(s)):
-            
-            if s[i] in "aeiou":
+            if s[i] in vowel:
                 curn +=1
-
+            
             if i < k-1:
                 continue
-        
+            
             maxn = max(maxn,curn)
 
-            if s[i-k+1] in "aeiou":
+            if s[i-k+1] in vowel:
                 curn -=1
         
         return maxn
+            
