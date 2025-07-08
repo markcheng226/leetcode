a6cabe3b9170e93adb07502deba469a1014e6e86
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-07 22:13:48 -0400

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.py
@@ -0,0 +1,18 @@
+# Last updated: 7/7/2025, 10:13:48 PM
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count = defaultdict(int)
+        left = 0
+        ans = 0
+
+        for i in range(len(fruits)):
+            count[fruits[i]] +=1
+
+            while len(count) > 2:
+                count[fruits[left]] -=1
+                if count[fruits[left]] == 0:
+                    del count[fruits[left]]
+                left +=1
+            
+            ans = max(ans,i-left+1)
+        return ans
