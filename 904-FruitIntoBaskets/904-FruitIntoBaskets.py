a53fca4f9f87ce2732aa1c5c379fa71738b72526
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-08 00:51:10 -0400

    File updated at 7/8/2025, 12:51:10 AM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.py
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.py
@@ -1,4 +1,4 @@
-# Last updated: 7/7/2025, 10:57:26 PM
+# Last updated: 7/8/2025, 12:51:10 AM
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         count = defaultdict(int)
@@ -9,10 +9,10 @@ class Solution:
             count[fruits[i]] +=1
 
             while len(count) >2:
-                count[fruits[left]] -=1
+                count[fruits[left]]-=1
                 if count[fruits[left]] == 0:
                     del count[fruits[left]]
                 left +=1
-        
-            res = max(res, i-left+1)
+            
+            res = max(res,i-left+1)
         return res
