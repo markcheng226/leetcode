328f151b829ace2ccc4ca195eed1f4e8f4f5f511
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-08 00:18:20 -0400

    File updated at 7/8/2025, 12:18:20 AM

--- 159-LongestSubstringwithAtMostTwoDistinctCharacters/159-LongestSubstringwithAtMostTwoDistinctCharacters.py
+++ 159-LongestSubstringwithAtMostTwoDistinctCharacters/159-LongestSubstringwithAtMostTwoDistinctCharacters.py
@@ -1,14 +1,14 @@
-# Last updated: 7/7/2025, 11:56:06 PM
+# Last updated: 7/8/2025, 12:18:20 AM
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        count = defaultdict(int)
         left = 0
         res = 0
-        count = defaultdict(int)
 
         for i in range(len(s)):
             count[s[i]] +=1
-            
-            while len(count)>2:
+
+            while len(count) > 2:
                 count[s[left]]-=1
                 if count[s[left]] == 0:
                     del count[s[left]]
