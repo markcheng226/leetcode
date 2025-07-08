4c08b1683e69b74b7b5a63bcf344350c60d9f743
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-08 00:52:32 -0400

    File updated at 7/8/2025, 12:52:32 AM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.py
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.py
@@ -1,9 +1,9 @@
-# Last updated: 7/7/2025, 11:20:37 PM
+# Last updated: 7/8/2025, 12:52:32 AM
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        zero = 0
         left = 0
-        res = 0 
+        zero = 0
+        res = 0
 
         for i in range(len(nums)):
             if nums[i] == 0:
@@ -11,8 +11,8 @@ class Solution:
             
             while zero > k:
                 if nums[left] == 0:
-                    zero -=1
+                    zero-=1
                 left +=1
-
+            
             res = max(res,i-left+1)
         return res
