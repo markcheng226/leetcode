78a2da14c9a50b1e9c15dd7d08b0071b1876af96
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-08 22:08:37 -0400

    File updated at 7/8/2025, 10:08:37 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,14 +1,18 @@
+# Last updated: 7/8/2025, 10:08:37 PM
 class Solution:
-    def isPalindrome(self,s):
-        l,r = 0,len(s)-1
+    def isPalindrome(self, s: str) -> bool:
+        l = 0 
+        r = len(s)-1
 
-        while l < r :
+
+        while l < r:
             while l < r and not s[l].isalnum():
                 l +=1
             while l < r and not s[r].isalnum():
                 r -=1
-            if s[l].lower() != s[r].lower():
+            
+            if s[l].lower()!= s[r].lower():
                 return False
-            l+=1
-            r-=1
-        return True        
+            l +=1
+            r -=1
+        return True
