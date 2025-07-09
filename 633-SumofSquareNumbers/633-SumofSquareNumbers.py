0b7e391a123c8b4e7bfea0b2f322f7b1b3c96576
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-08 23:52:33 -0400

    Create 633-SumofSquareNumbers

---
+++ 633-SumofSquareNumbers/633-SumofSquareNumbers.py
@@ -0,0 +1,15 @@
+# Last updated: 7/8/2025, 11:52:32 PM
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = 0
+        right = int(math.isqrt(c))
+
+        while left <= right:
+            total = left*left + right*right
+            if total > c:
+                right -=1
+            elif total < c:
+                left +=1
+            else:
+                return True
+        return False
