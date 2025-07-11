e06140a51907d121d92b52772435c775c171a664
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-10 23:09:48 -0400

    File updated at 7/10/2025, 11:09:48 PM

--- 1868-ProductofTwoRun-LengthEncodedArrays/1868-ProductofTwoRun-LengthEncodedArrays.py
+++ 1868-ProductofTwoRun-LengthEncodedArrays/1868-ProductofTwoRun-LengthEncodedArrays.py
@@ -1,4 +1,4 @@
-# Last updated: 7/10/2025, 9:24:07 PM
+# Last updated: 7/10/2025, 11:09:48 PM
 class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
         i,j = 0,0
@@ -8,19 +8,20 @@ class Solution:
             v1,f1 = encoded1[i]
             v2,f2 = encoded2[j]
 
-            prod = v1*v2
+            prod = v1 *v2
             freq = min(f1,f2)
 
             if res and res[-1][0] == prod:
-                res[-1][1] += freq
+                res[-1][1]+= freq
             else:
                 res.append([prod,freq])
-            
-            encoded1[i][1] -= freq
-            encoded2[j][1] -=freq
 
-            if encoded1[i][1] == 0:
+            encoded1[i][1] -= freq
+            encoded2[j][1] -= freq
+            
+            if encoded1[i][1]== 0:
                 i +=1
+            
             if encoded2[j][1] == 0:
                 j +=1
         return res
