7f66e15575489d021ad979aa0043d2892f9caf42
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-10 21:36:47 -0400

    File updated at 7/10/2025, 9:36:47 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,22 +1,20 @@
-# Last updated: 7/10/2025, 5:42:23 PM
+# Last updated: 7/10/2025, 9:36:47 PM
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low = 0 
-        mid = 0
+        low, mid = 0,0
         high = len(nums)-1
 
         while mid <= high:
             if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low +=1
+                nums[mid],nums[low] = nums[low],nums[mid]
                 mid +=1
+                low +=1
+
             elif nums[mid] == 1:
                 mid +=1
             else:
                 nums[mid],nums[high] = nums[high],nums[mid]
                 high -=1
-
-
