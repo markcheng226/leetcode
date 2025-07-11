0a601a6a9ada12a7eb24309522656e5411833175
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-10 21:31:02 -0400

    Create 1446-ConsecutiveCharacters

---
+++ 1446-ConsecutiveCharacters/1446-ConsecutiveCharacters.py
@@ -0,0 +1,15 @@
+# Last updated: 7/10/2025, 9:31:01 PM
+class Solution:
+    def maxPower(self, s: str) -> int:
+        max_len = 0
+        count = 0
+
+        for i in range(0,len(s)):
+            if s[i] == s[i-1]:
+                count +=1
+            elif s[i] != s[i-1]:
+                count = 1
+            else:
+                count = 1
+            max_len = max(max_len,count)
+        return max_len
