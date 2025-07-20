9d08afc4d1d723ed44f1e4587681ebbf70418eda
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-20 00:18:28 -0400

    File updated at 7/20/2025, 12:18:28 AM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,13 +1,13 @@
-# Last updated: 7/20/2025, 12:05:20 AM
+# Last updated: 7/20/2025, 12:18:28 AM
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights),len(heights[0])
+        rows,cols = len(heights),len(heights[0])
         pac = set()
         atl = set()
         directions = [1,0],[-1,0],[0,1],[0,-1]
 
-        def dfs(r,c,visited,prev_height):
-            if r<0 or c<0 or r>= rows or c>=cols or (r,c) in visited or heights[r][c] < prev_height:
+        def dfs(r,c,visited,prev_heights):
+            if r <0 or c<0 or r>=rows or c>=cols or (r,c) in visited or heights[r][c] < prev_heights :
                 return
             
             visited.add((r,c))
@@ -21,11 +21,10 @@ class Solution:
         for c in range(cols):
             dfs(0,c,pac,heights[0][c])
             dfs(rows-1,c,atl,heights[rows-1][c])
-
-        result = []
-
+        
+        res = []
         for r in range(rows):
             for c in range(cols):
                 if (r,c) in pac and (r,c) in atl:
-                    result.append([r,c])
-        return result
+                    res.append([r,c])
+        return res
