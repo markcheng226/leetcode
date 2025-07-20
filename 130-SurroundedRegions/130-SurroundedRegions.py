0451bb99b339aaa242d4046f3989246c6f6c9f44
Mark Cheng <115590620+markcheng226@users.noreply.github.com>
2025-07-19 23:05:34 -0400

    Create 130-SurroundedRegions

---
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -0,0 +1,32 @@
+# Last updated: 7/19/2025, 11:05:34 PM
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows,cols = len(board),len(board[0])
+        directions = [1,0],[-1,0],[0,1],[0,-1]
+        
+        def dfs(r,c):
+            if r<0 or c<0 or r>=rows or c>=cols or board[r][c] != "O":
+                return
+            board[r][c] = "#"
+
+            for dr,dc in directions:
+                dfs(r+dr,c+dc)
+            
+        for r in range(rows):
+            dfs(r,0)
+            dfs(r,cols-1)
+        for c in range(cols):
+            dfs(0,c)
+            dfs(rows-1,c)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "#":
+                    board[r][c] = "O"
+
+        
