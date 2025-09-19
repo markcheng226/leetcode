691d8e1a689c6e401dded3e76fc93555938cdbb6
Mark(Muyang) Cheng <115590620+markcheng226@users.noreply.github.com>
2025-09-19 01:31:35 -0400

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/162-FindPeakElement.py
@@ -0,0 +1,16 @@
+# Last updated: 9/19/2025, 1:31:34 AM
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+
+        if len(nums) == 1:
+            return 0
+
+        while l < r:
+            mid = (l+r) //2
+            if nums[mid] < nums[mid+1]:
+                l = mid +1
+            else:
+                r = mid
+        return l
+
