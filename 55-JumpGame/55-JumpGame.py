6b65d56081c3f949be2d97751d40f0adf4457a48
Mark(Muyang) Cheng <115590620+markcheng226@users.noreply.github.com>
2025-09-30 03:15:17 -0400

    File updated at 9/30/2025, 3:15:16 AM

--- 55-JumpGame/55-JumpGame.py
+++ 55-JumpGame/55-JumpGame.py
@@ -1,9 +1,9 @@
-# Last updated: 9/30/2025, 3:14:14 AM
+# Last updated: 9/30/2025, 3:15:16 AM
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums)-1
         
-        for i in range(len(nums)-1,-1,-1):
+        for i in range(len(nums)-2,-1,-1):
             if i + nums[i] >= goal:
                 goal = i
         return True if goal == 0 else False
