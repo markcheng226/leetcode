3a5421c66ddcd727a9edbe96a1101564eb933618
Mark(Muyang) Cheng <115590620+markcheng226@users.noreply.github.com>
2025-09-30 20:43:31 -0400

    File updated at 9/30/2025, 8:43:31 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,12 +1,15 @@
-# Last updated: 9/30/2025, 4:06:59 AM
+# Last updated: 9/30/2025, 8:43:31 PM
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        far,end,res = 0,0,0
+        res= 0
+        cur_end = 0
+        farest =0
+
         for i in range(len(nums)-1):
-            far = max(far,nums[i]+i)
-            if i == end:
+            farest = max(farest,i+nums[i])
+            if i == cur_end:
                 res +=1
-                end = far
-                if end >= len(nums)-1:
+                cur_end = farest
+                if cur_end >= len(nums)-1:
                     break
         return res
