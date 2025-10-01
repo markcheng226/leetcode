1214ac50fd18ec479b655a316f51a2c051f5e510
Mark(Muyang) Cheng <115590620+markcheng226@users.noreply.github.com>
2025-09-30 21:43:41 -0400

    File updated at 9/30/2025, 9:43:40 PM

--- 274-H-Index/274-H-Index.py
+++ 274-H-Index/274-H-Index.py
@@ -1,17 +1,20 @@
-# Last updated: 9/30/2025, 9:07:25 PM
+# Last updated: 9/30/2025, 9:43:40 PM
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        buckets = [0] * (n + 1)
-        for c in citations:
-            if c >= n:
-                buckets[n] += 1
-            else:
-                buckets[c] += 1
 
-        papers = 0
-        for h in range(n, -1, -1):
-            papers += buckets[h]
-            if papers >= h:
-                return h
+        buckets = [0] * (n+1)
+
+        for i in range(len(citations)):
+            c = citations[i]
+            if c >= n:
+                buckets[n] +=1
+            else:
+                buckets[c] +=1
+        
+        paper = 0
+        for i in range(n,-1,-1):
+            paper += buckets[i]
+            if paper >= i:
+                return i
         return 0
