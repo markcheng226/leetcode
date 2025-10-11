171052ad10c21c2296b5f3ef7a3b7e4b68ae019e
Muyang Cheng <115590620+markcheng226@users.noreply.github.com>
2025-10-10 22:18:04 -0400

    File updated at 10/10/2025, 10:18:04 PM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,3 +1,4 @@
+# Last updated: 10/10/2025, 10:18:04 PM
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = defaultdict(set)
@@ -9,11 +10,11 @@ class Solution:
                 if board[r][c] == ".":
                     continue
                 
-                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3,c//3)]):
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3),(c//3)]:
                     return False
-                
                 else:
                     rows[r].add(board[r][c])
                     cols[c].add(board[r][c])
-                    squares[(r//3,c//3)].add(board[r][c])
+                    squares[(r//3),(c//3)].add(board[r][c])
         return True
+
