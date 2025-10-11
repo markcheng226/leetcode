06c65fdee262f78e7f8a91b5a931761f6808ca88
Muyang Cheng <115590620+markcheng226@users.noreply.github.com>
2025-10-10 20:03:12 -0400

    File updated at 10/10/2025, 8:03:12 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,20 +1,20 @@
-# Last updated: 10/10/2025, 8:02:06 PM
+# Last updated: 10/10/2025, 8:03:12 PM
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+
         if len(s) != len(t):
             return False
-
+        
         count = {}
         for i in t:
             count[i] = count.get(i,0) +1
-
+        
         for i in s:
             if i not in count:
                 return False
-            else:
-                count[i] -=1
-            
+            count[i] -=1
+
             if count[i] < 0:
                 return False
         return True
+
