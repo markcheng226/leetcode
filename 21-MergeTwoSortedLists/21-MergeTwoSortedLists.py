b22fc3f558b76cffd7f3490e9bfb15e3bf61c3e4
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-16 21:11:57 -0400

    File updated at 10/16/2025, 9:11:56 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,3 +1,4 @@
+# Last updated: 10/16/2025, 9:11:56 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,13 +10,12 @@ class Solution:
         tail = dummy
 
         while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
+            if list1.val > list2.val:
                 tail.next = list2
                 list2 = list2.next
-            
+            else:
+                tail.next = list1
+                list1 = list1.next
             tail = tail.next
         tail.next = list1 or list2
         return dummy.next
