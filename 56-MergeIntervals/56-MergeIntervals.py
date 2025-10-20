da2034e2267470f0bf4bd5ee80062dd619369b1b
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-19 21:42:42 -0400

    File updated at 10/19/2025, 9:42:42 PM

--- 56-MergeIntervals/56-MergeIntervals.py
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -1,16 +1,19 @@
-# Last updated: 10/19/2025, 9:08:24 PM
+# Last updated: 10/19/2025, 9:42:42 PM
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
+        
+        res = []
 
         intervals.sort(key=lambda x:x[0])
 
-        res = []
+        
 
         for start,end in intervals:
             if not res or start > res[-1][1]:
                 res.append([start,end])
             else:
-                res[-1][1]=max(res[-1][1],end)
+                res[-1][1] = max(res[-1][1],end)
+        
         return res
