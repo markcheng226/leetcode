df3ce73733077ce6f714098eee23b7f3d4f268ee
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-19 20:22:07 -0400

    File updated at 10/19/2025, 8:22:07 PM

--- 253-MeetingRoomsII/253-MeetingRoomsII.py
+++ 253-MeetingRoomsII/253-MeetingRoomsII.py
@@ -1,4 +1,4 @@
-# Last updated: 10/19/2025, 7:42:16 PM
+# Last updated: 10/19/2025, 8:22:07 PM
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals:
@@ -8,14 +8,15 @@ class Solution:
         ends = sorted(i[1] for i in intervals)
 
         i=j=0
-        room = res = 0
-        
+        res = 0
+        room =0
+
         while i < len(intervals):
             if starts[i] < ends[j]:
                 room +=1
-                res = max(res,room)
                 i +=1
+                res = max(res,room)
             else:
                 room -=1
-                j +=1
+                j+=1
         return res
