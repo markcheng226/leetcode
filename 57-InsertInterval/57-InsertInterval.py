5ad90bf83396992e966c235e3eb2ea62b889ceed
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-19 22:25:04 -0400

    File updated at 10/19/2025, 10:25:04 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,20 +1,21 @@
-# Last updated: 10/19/2025, 10:22:20 PM
+# Last updated: 10/19/2025, 10:25:04 PM
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
         i = 0
+        n = len(intervals)
 
-        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+        while i < n and intervals[i][1] < newInterval[0]:
             res.append(intervals[i])
             i +=1
-
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+        
+        while i < n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0],intervals[i][0])
             newInterval[1] = max(newInterval[1],intervals[i][1])
             i +=1
         res.append(newInterval)
 
-        while i < len(intervals):
+        while i < n:
             res.append(intervals[i])
             i +=1
         
