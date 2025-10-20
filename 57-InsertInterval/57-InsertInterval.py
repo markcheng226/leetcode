bc442cdb3d2c3dbcc3c0bc6ee3a6a407b96b16c3
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-19 22:22:21 -0400

    File updated at 10/19/2025, 10:22:20 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,25 +1,21 @@
-# Last updated: 10/19/2025, 7:22:20 PM
+# Last updated: 10/19/2025, 10:22:20 PM
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
         i = 0
-        n = len(intervals)
 
-        # 1. 左边完全不重叠的部分
-        while i < n and intervals[i][1] < newInterval[0]:
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
             res.append(intervals[i])
-            i += 1
+            i +=1
 
-        # 2. 重叠部分合并
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0],intervals[i][0])
+            newInterval[1] = max(newInterval[1],intervals[i][1])
+            i +=1
         res.append(newInterval)
 
-        # 3. 右边完全不重叠的部分
-        while i < n:
+        while i < len(intervals):
             res.append(intervals[i])
-            i += 1
-
+            i +=1
+        
         return res
