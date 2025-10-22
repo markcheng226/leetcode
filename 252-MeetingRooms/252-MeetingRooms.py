aedccf40690a3c7b5b90fddf7222554e304b0288
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-22 00:10:07 -0400

    File updated at 10/22/2025, 12:10:07 AM

--- 252-MeetingRooms/252-MeetingRooms.py
+++ 252-MeetingRooms/252-MeetingRooms.py
@@ -1,11 +1,12 @@
-# Last updated: 10/22/2025, 12:09:08 AM
+# Last updated: 10/22/2025, 12:10:07 AM
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort(key=lambda x:x[0])
-        
+
         prev_end = float("-inf")
+
         for start,end in intervals:
-            if start < prev_end:
+            if start<prev_end:
                 return False
             else:
                 prev_end = end
