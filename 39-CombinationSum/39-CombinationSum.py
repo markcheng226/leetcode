5ca46f8c78d309f11452a0876429a2851b38c3a6
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-22 00:31:09 -0400

    File updated at 10/22/2025, 12:31:09 AM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,19 +1,19 @@
-# Last updated: 10/22/2025, 12:30:11 AM
+# Last updated: 10/22/2025, 12:31:09 AM
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res =[]
+        res = []
         candidates.sort()
 
-        def backtrack(i,cur,total):
-            if total == target:
+        def backtrack(i, cur, remain):
+            if remain == 0:
                 res.append(cur.copy())
-            
-            for j in range(i,len(candidates)):
-                if total + candidates[j] > target:
+                return
+            for j in range(i, len(candidates)):
+                if candidates[j] > remain:
                     break
-                else:
-                    cur.append(candidates[j])
-                    backtrack(j,cur,total +candidates[j])
-                    cur.pop()
-        backtrack(0,[],0)
+                cur.append(candidates[j])
+                backtrack(j, cur, remain - candidates[j])
+                cur.pop()
+        
+        backtrack(0, [], target)
         return res
