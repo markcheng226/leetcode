647df98917f93a15f583d1e77f72c765374557a6
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-26 19:33:48 -0400

    File updated at 10/26/2025, 7:33:48 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,25 +1,23 @@
-# Last updated: 10/26/2025, 7:32:19 PM
+# Last updated: 10/26/2025, 7:33:48 PM
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n, m = len(s1), len(s2)
         if n > m:
             return False
-
-        def idx(c): return ord(c) - 97
-
+            
         need = [0] * 26
         win = [0] * 26
         for c in s1:
-            need[idx(c)] += 1
+            need[ord(c) - 97] += 1
 
         for i in range(n):
-            win[idx(s2[i])] += 1
+            win[ord(s2[i]) - 97] += 1
         if win == need:
             return True
 
         for r in range(n, m):
-            win[idx(s2[r])] += 1
-            win[idx(s2[r - n])] -= 1
+            win[ord(s2[r]) - 97] += 1
+            win[ord(s2[r-n]) - 97] -= 1
             if win == need:
                 return True
         return False
