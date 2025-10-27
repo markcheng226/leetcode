024238f54d42dcc65574baca363146a969944510
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-26 20:29:46 -0400

    File updated at 10/26/2025, 8:29:46 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,34 +1,32 @@
-# Last updated: 10/13/2025, 11:01:21 PM
+# Last updated: 10/26/2025, 8:29:46 PM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t or not s:
+        if not t or not s or len(t) > len(s):
             return ""
         
-        need = {}
-        for i in t:
-            need [i] = need.get(i,0) +1
-        
-        miss = len(t)
-        left = 0
-        bestlen = float("inf")
-        bestleft = 0
+        need = Counter(t)
+        missing = len(t)
+        l = 0
+        best_len = float("inf")
+        best_l = 0
 
         for i in range(len(s)):
-            c = s[i]
-            if c in need:
-                if need[c] > 0:
-                    miss -=1
-                need[c] -=1
+            ch = s[i]
+            need[ch] -=1
+            if need[ch] >= 0:
+                missing -=1
+            
 
-            while miss == 0:
-                if i -left+1<bestlen:
-                    bestlen = i-left +1
-                    bestleft = left
+            while missing == 0:
+                if i-l+1<best_len:
+                    best_len = i-l+1
+                    best_l = l
                 
-                cl = s[left]
-                if cl in need:
-                    need[cl] +=1
-                    if need[cl] > 0:
-                        miss +=1
-                left +=1
-        return "" if bestlen == float("inf") else s[bestleft:bestleft+bestlen]
+                left_ch = s[l]
+                need[left_ch]+=1
+                l +=1
+                if need[left_ch] > 0:
+                    missing +=1
+        return "" if best_len == float("inf") else s[best_l:best_l+best_len]
+
+
