551420f8470dada24bbc3621a2732370ca171a46
Muyang Cheng <115590620+muyangcheng329@users.noreply.github.com>
2025-10-26 20:39:31 -0400

    File updated at 10/26/2025, 8:39:31 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,4 +1,4 @@
-# Last updated: 10/26/2025, 8:29:46 PM
+# Last updated: 10/26/2025, 8:39:31 PM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if not t or not s or len(t) > len(s):
@@ -6,27 +6,24 @@ class Solution:
         
         need = Counter(t)
         missing = len(t)
-        l = 0
+        l=0
         best_len = float("inf")
         best_l = 0
 
         for i in range(len(s)):
-            ch = s[i]
-            need[ch] -=1
-            if need[ch] >= 0:
+            need[s[i]] -=1
+            if need[s[i]] >= 0:
                 missing -=1
             
-
-            while missing == 0:
+            while missing ==0:
                 if i-l+1<best_len:
                     best_len = i-l+1
-                    best_l = l
-                
-                left_ch = s[l]
-                need[left_ch]+=1
-                l +=1
-                if need[left_ch] > 0:
+                    best_l=l
+            
+                need[s[l]]+=1
+                if need[s[l]] > 0:
                     missing +=1
+                l +=1
         return "" if best_len == float("inf") else s[best_l:best_l+best_len]
 
 
